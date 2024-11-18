ad5f54df9954917446ca81645d935071a15b12ce
100gammmit <100minha@naver.com>
2024-11-18 17:47:00 +0900

    gitprac

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("== 명언 앱 ==");
+
+    }
+
+    private class Wisesaying {
+        int Id;
+        String Saying;
+        String Writter;
+
+    }
+}
